ecc975dc31d8a39576cf3d64a4074e171cc79f47
Nilson Jacques <neil.jacques@gmail.com>
2017-04-07 07:44:03 -0300

    Add additional event fields

--- models/event.js
+++ models/event.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  title: { type: String, required: [true, 'A title is required'] },
+  type: { type: String, required: [true, 'A type is required'] },
   date: { type: Date, required: [true, 'A date is required'] },
+  title: { type: String, required: [true, 'A title is required'] },
+  speaker: { type: String, required: [true, 'A speaker is required'] },
+  host: { type: String, required: [true, 'A host is required'] },
   createdAt: { type: Date, default: Date.now },
 });
 
