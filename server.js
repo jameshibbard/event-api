e2300e717b94003df896b039b3c0b2451ce37cd9
Nilson Jacques <neil.jacques@gmail.com>
2017-04-07 07:42:03 -0300

    Replace manual CORS headers with cors module

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('./config/database');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-   .use(bodyParser.json());
+app.use(cors())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json());
 
 app.use('/api', require('./routes/event'));
 
